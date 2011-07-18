ec03436b5640aa0eb7dd1d63067a2164e50fea2e
Audrey M Roy <audreyr@gmail.com>
2011-07-18 13:53:19 -0700

    Added accounts app.

---
+++ accounts/backends.py
@@ -0,0 +1,94 @@
+from django.conf import settings
+from django.contrib.sites.models import RequestSite
+from django.contrib.sites.models import Site
+
+from regplus import signals
+from regplus.models import RegistrationProfile
+
+from accounts.forms import RegistrationForm
+
+from profiles.models import Profile
+
+class StockRegistrationBackend(object):
+
+    def register(self, request, **kwargs):
+
+        username, email, password = kwargs['username'], kwargs['email'], kwargs['password1']
+        first_name, last_name = kwargs['first_name'], kwargs['last_name']
+        if Site._meta.installed:
+            site = Site.objects.get_current()
+        else:
+            site = RequestSite(request)
+
+        new_user = RegistrationProfile.objects.create_inactive_user(username, email,
+                                password, site, send_email=settings.ACCOUNTS_ACTIVATION_EMAIL)
+
+        signals.user_registered.send(sender=self.__class__,
+                                     user=new_user,
+                                     request=request)
+
+        profile = Profile.objects.create(
+            user=new_user,
+            first_name=first_name,
+            last_name=last_name,
+            email = new_user.email
+        )
+        profile.save()
+      
+        return new_user
+
+    def activate(self, request, activation_key):
+        """
+        Given an an activation key, look up and activate the user
+        account corresponding to that key (if possible).
+
+        After successful activation, the signal
+        ``registration.signals.user_activated`` will be sent, with the
+        newly activated ``User`` as the keyword argument ``user`` and
+        the class of this backend as the sender.
+        
+        """
+        activated = RegistrationProfile.objects.activate_user(activation_key)
+        if activated:
+            signals.user_activated.send(sender=self.__class__,
+                                        user=activated,
+                                        request=request)
+        return activated
+
+    def registration_allowed(self, request):
+        """
+        Indicate whether account registration is currently permitted,
+        based on the value of the setting ``REGISTRATION_OPEN``. This
+        is determined as follows:
+
+        * If ``REGISTRATION_OPEN`` is not specified in settings, or is
+          set to ``True``, registration is permitted.
+
+        * If ``REGISTRATION_OPEN`` is both specified and set to
+          ``False``, registration is not permitted.
+        
+        """
+        return getattr(settings, 'REGISTRATION_OPEN', True)
+
+    def get_form_class(self, request):
+        """
+        Return the default form class used for user registration.
+        
+        """
+        return RegistrationForm
+
+    def post_registration_redirect(self, request, user):
+        """
+        Return the name of the URL to redirect to after successful
+        user registration.
+        
+        """
+        return ('registration_complete', (), {})
+
+    def post_activation_redirect(self, request, user):
+        """
+        Return the name of the URL to redirect to after successful
+        account activation.
+        
+        """
+        return ('registration_activation_complete', (), {})
---
+++ accounts/urls.py
@@ -0,0 +1,39 @@
+from django.conf import settings
+from django.conf.urls.defaults import patterns, include, url
+from django.views.generic.simple import direct_to_template
+
+from regplus.views import activate
+from regplus.views import register
+
+from accounts.forms import RegistrationForm
+
+
+urlpatterns = patterns('',
+
+       url(r'^register/$',
+           register,
+           {'backend': "accounts.backends.StockRegistrationBackend",
+           'form_class': RegistrationForm},
+           name='registration_register'),
+       url(r'^activate/complete/$',
+           direct_to_template,
+           {'template': 'registration/activation_complete.html'},
+           name='registration_activation_complete'),
+       # Activation keys get matched by \w+ instead of the more specific
+       # [a-fA-F0-9]{40} because a bad activation key should still get to the view;
+       # that way it can return a sensible "invalid key" message instead of a
+       # confusing 404.
+       url(r'^activate/(?P<activation_key>\w+)/$',
+           activate,
+           {'backend': "accounts.backends.StockRegistrationBackend"},
+           name='registration_activate'),           
+       url(r'^register/complete/$',
+           direct_to_template,
+           {'template': 'registration/registration_complete.html'},
+           name='registration_complete'),
+       url(r'^register/closed/$',
+           direct_to_template,
+           {'template': 'registration/registration_closed.html'},
+           name='registration_disallowed'),
+       (r'', include('registration.auth_urls')),
+)
