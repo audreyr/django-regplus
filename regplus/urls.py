3010c6da5e91eee5e7f2f744e4537e9c5c7af70f
Audrey M Roy <audreyr@gmail.com>
2011-07-18 14:06:26 -0700

    More fixing name change broken things

--- regplus/urls.py
+++ regplus/urls.py
@@ -12,4 +12,4 @@ import warnings
 warnings.warn("include('registration.urls') is deprecated; use include('registration.backends.default.urls') instead.",
               PendingDeprecationWarning)
 
-from registration.backends.default.urls import *
+from regplus.backends.default.urls import *
